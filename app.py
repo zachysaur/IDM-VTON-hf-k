a64dc065598068b4a48a8fa320504612bd6919ee
zachysaur <124980322+zachysaur@users.noreply.github.com>
2024-05-11 13:01:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -302,8 +302,16 @@ with image_blocks as demo:
                 denoise_steps = gr.Number(label="Denoising Steps", minimum=20, maximum=40, value=30, step=1)
                 seed = gr.Number(label="Seed", minimum=-1, maximum=2147483647, step=1, value=42)
 
-
-
     try_button.click(fn=start_tryon, inputs=[imgs, garm_img, prompt, is_checked,is_checked_crop, denoise_steps, seed], outputs=[image_out,masked_img], api_name='tryon')
 
-
+url = gr.Interface(
+    [start_tryon],
+    image_blocks,
+    "blocks",
+    title="IDM-VTON Virtual Try-On",
+    show_input=False,
+    show_output=False,
+    capture_session=True
+)
+print("Local URL: http://127.0.0.1:{}".format(url.test_port))
+url.launch()
